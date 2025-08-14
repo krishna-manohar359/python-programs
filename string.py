4d3a5c6271eae2416719c5071b42a41c700011d2
krishna-manohar359 <krishnamanoharnandu@gmail.com>
2025-08-14 19:56:57 +0530

    Update string.py

--- string.py
+++ string.py
@@ -6,4 +6,6 @@ print(str2*3)#print numberr of times
 print(str1[0])#index of string
 print(str2[1*3])#mathemetical expreesion like multplying index with a value
 print(str1[1:4])#range with index
-print(str1.center(12,))
+print(str1.center(12))
+print(min(str1))
+print(str1.startswith("hello",1,len(str1)))
