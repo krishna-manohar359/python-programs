e9daf0b5d91cdb3a2bd9107654023763e956db09
krishna-manohar359 <krishnamanoharnandu@gmail.com>
2025-08-16 14:19:51 +0530

    Update set.py

--- set.py
+++ set.py
@@ -1,8 +1,21 @@
-set1={10,True,"krishna",98,12,-2,0.13}
-#set2={10}
-#set1.add(123)
-#set[1]=45//not accesing index
-#print(set1.issuperset)
-#print(set1.issubset(set2))
-#etc key words are there
-print(set1)
+t={1,2,3,4,5,7,8}
+s={1,2,4,7,5,3,6}
+#s.add(1)
+#s.update(t)
+#t.remove(3)
+#t.clear
+#t.pop
+#print(t.discard(3))
+#print(t)
+#print(s)
+print(max(s))
+print(max(t))
+print(min(s))
+print(min(t))
+print(s.issubset(t))#all elemnets in s should present in t
+print(s.issuperset(t))#all elemnets in t should present in s
+print(s.union(t))
+print(s.intersection(t))
+print(s.difference(t))#caluclate s difference
+print(t.difference(s))#caluclatest diiference
+print(s.symmetric_difference(t))#caluclates both difference
