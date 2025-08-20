733d5f854c360c23ca4ee9ee877b3f6403e157bd
krishna-manohar359 <krishnamanoharnandu@gmail.com>
2025-08-20 07:21:29 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,5 @@
+print("sum of two numbers and their product")
+#lambda is used for mathematical expressions
+sum=(lambda X,Y:X+Y)
+product=(lambda x,y:x*y)
+print("sum and product:/n",sum(5,3),product(5,3))
